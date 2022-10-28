eeb1cb76403e85223dc12350b567c77cb9879629
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-10-28 10:36:13 +0700

    sll ```add``` : addLast Method

--- bin/struktur_data.dart
+++ bin/struktur_data.dart
@@ -4,6 +4,7 @@ void main(List<String> arguments) {
   SingleLinkedList<int> data = SingleLinkedList();
   data.addFirst(9);
   data.addFirst(10);
+  data.addLast(8);
   for (int i = 0; i < data.size; i++) {
     print(data.getDataByIndex(i));
   }
--- lib/single_linked_list.dart
+++ lib/single_linked_list.dart
@@ -21,14 +21,32 @@ class SingleLinkedList<T> {
       print("Error null");
       return null;
     }
-    if (index >= size){
+    if (index >= size) {
+      print(size + index);
       print("Error index out of bounds");
       return null;
     }
     NodeLinkedList? curr = first;
     for (int i = 0; i < index; i++) {
       curr = curr?.next;
+      if (curr?.next == null) break;
     }
     return curr?.data;
+  }
+
+  bool addLast(T data) {
+    NodeLinkedList node = NodeLinkedList(data);
+    if (first == null) {
+      first = node;
+      size++;
+      return true;
+    }
+    NodeLinkedList? curr = first;
+    while (curr?.next != null) {
+      curr = curr?.next;
+    }
+    curr?.next = node;
+    size++;
+    return true;
   }
 }
